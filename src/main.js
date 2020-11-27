194009493e820aec91031d896fd56be5d180e43a
xiongjian <webjsforyou@gmail.com>
2020-11-27 13:56:31 +0800

    feat: 初始化仓库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+// @ts-nocheck
+import Vue from "vue";
+import App from "./App";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
