789fe3fe392889e6cf15b54e4984e3462353ae6c
xiongjian <webjsforyou@gmail.com>
2020-12-07 16:08:08 +0800

    feat(Zoom): 添加放大镜效果

--- src/views/Detail/Zoom/Zoom.vue
+++ src/views/Detail/Zoom/Zoom.vue
@@ -1,24 +1,69 @@
 <template>
   <div class="spec-preview">
-    <!-- 中图 -->
     <img :src="imgUrl" />
-    <!-- 和中图一样大小的div -->
-    <div class="event"></div>
-    <!-- 大图 -->
+    <div class="event" @mousemove="move" ref="event"></div>
+    <div class="mask" ref="mask"></div>
+
     <div class="big">
-      <img :src="bigImgUrl" />
+      <img :src="bigImgUrl" ref="bigImg" />
     </div>
-    <!-- 绿色的遮罩层 -->
-    <div class="mask"></div>
   </div>
 </template>
 
 <script>
+// 节流函数
+import throttle from "lodash/throttle";
+
 export default {
   name: "Zoom",
+
   props: {
-    imgUrl: String,
-    bigImgUrl: String,
+    imgUrl: String, // 中图的url
+    bigImgUrl: String, // 大图的url
+  },
+
+  mounted() {
+    // this.maskWidth = this.$refs.mask.clientWidth // 开始是隐藏的, 得不到宽度
+    // console.log('maskWidth', this.maskWidth)
+    this.maskWidth = this.$refs.event.clientWidth / 2;
+  },
+
+  methods: {
+    move: throttle(function (event) {
+      console.log("-----");
+
+      let left, top;
+      const maskDiv = this.$refs.mask;
+      const bigImg = this.$refs.bigImg;
+
+      // 取出相关的数据: 事件的offsetX/offsetY, mask <div>的宽度maskWidth
+      const { offsetX, offsetY } = event;
+      // const maskWidth = maskDiv.clientWidth
+      const maskWidth = this.maskWidth;
+
+      // 计算left, top
+      left = offsetX - maskWidth / 2;
+      top = offsetY - maskWidth / 2;
+      // left和top必须在[0, maskWidth]区间内
+      if (left < 0) {
+        left = 0;
+      } else if (left > maskWidth) {
+        left = maskWidth;
+      }
+      if (top < 0) {
+        top = 0;
+      } else if (top > maskWidth) {
+        top = maskWidth;
+      }
+
+      // 指定mask <div>的坐标值(left, top)
+      maskDiv.style.left = left + "px";
+      maskDiv.style.top = top + "px";
+
+      // 指定大图 <img>的坐标值(left, top)
+      bigImg.style.left = -2 * left + "px";
+      bigImg.style.top = -2 * top + "px";
+    }, 50),
   },
 };
 </script>
