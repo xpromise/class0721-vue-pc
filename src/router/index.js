6ebd3b1db041caea83f213136c7f5f9379172c50
xiongjian <webjsforyou@gmail.com>
2020-12-08 09:56:26 +0800

    feat: 前端路由前置守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "../store";
 
 import Home from "../views/Home";
 import Login from "../views/Login";
@@ -40,7 +41,7 @@ VueRouter.prototype.replace = function(location, onComplete, onAbort) {
 // 安装插件
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
 	// 路由配置
 	routes: [
 		{
@@ -118,3 +119,55 @@ export default new VueRouter({
 		return { x: 0, y: 0 };
 	},
 });
+
+/*
+	路由守卫：
+		1. 是什么
+			在路由跳转之前、之中、之后触发的钩子函数（类似于生命周期）
+		2. 分类
+			全局守卫
+				全局前置守卫：在切换路由之前触发
+					beforeEach
+				全局解析守卫：之中
+					beforeResolve
+				全局后置守卫：之后
+					afterEach
+			路由守卫
+			组件守卫		
+
+*/
+
+// 需要进行权限验证的地址
+const permissionPaths = ["/trade", "/pay", "/center"];
+// 路由全局前置守卫
+router.beforeEach((to, from, next) => {
+	/*
+		to   要去的路由对象($route)
+		from 从哪来的路由对象（当前路由对象）($route)
+		next 是一个函数：跳转到哪个路由的方法
+			比如：要去to这个路由 next()	
+						要去登录路由 next('/login')  next({path: '/login'})  next({name: 'login'})
+	
+		
+		权限验证：
+			如果用户未登录，不允许去 trade pay center 等路由
+	*/
+
+	// if (permissionPaths.indexOf(to.path) > -1) {
+	// 	if (store.state.user.token) {
+	// 		next();
+	// 	} else {
+	// 		next("/login");
+	// 	}
+	// } else {
+	// 	next();
+	// }
+
+	if (permissionPaths.indexOf(to.path) > -1 && !store.state.user.token) {
+		return next("/login");
+	}
+
+	next();
+});
+
+export default router;
